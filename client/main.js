e0ba9e3a9ae6dd1351d16d09d598fdee8e288886
Jeff Bryner <jeff@jeffbryner.com>
2018-06-28 13:09:44 -0700

    minimal js main

--- client/main.js
+++ client/main.js
@@ -1,1 +1,17 @@
-console.log(`Greetings from ${module.id}!`);
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Mongo } from 'meteor/mongo';
+import { Session } from 'meteor/session';
+import { Tracker } from 'meteor/tracker';
+import { _ } from 'meteor/underscore';
+import { Blaze } from 'meteor/blaze';
+import { $ } from 'meteor/jquery';
+import 'meteor/jeffbryner:quill-reactive';
+import {quillReactive} from 'meteor/jeffbryner:quill-reactive';
+import './main.html';
+
+if (Meteor.isClient) {
+    notes = new Mongo.Collection('notes');
+    Session.set("liveEditing",true);
+}
