dfbe19d57108333006b8028c5a6587a9235e6d3f
Jeff Bryner <jeff@jeffbryner.com>
2018-06-28 13:50:25 -0700

    auto reset the collection

--- server/main.js
+++ server/main.js
@@ -5,12 +5,6 @@ import { Session } from 'meteor/session';
 Meteor.startup(() => {
     // code to run on server at startup
     notes = new Mongo.Collection('notes');
-    notes.remove({});
-    notes.insert({
-        _id: '1234',
-        note: 'starter text'
-    });
-
     notes.allow({
         insert: function (userId, doc) {
             return (true);
@@ -23,4 +17,24 @@ Meteor.startup(() => {
         },
         fetch: ['creator']
     });
+    defaultNote = {
+        _id: '1234',
+        note: "<p>You can type anything here</p><p>with <strong>formatting</strong></p><ul><li>lists</li><li>etc</li></ul><p>And anything else from other folks on this webpage should show up in realtime!</p>",
+        noteDelta: {ops: [
+            { "insert" : "You can type anything here\nwith " }, { "attributes" : { "bold" : true },
+             "insert" : "formatting" }, { "insert" : "\nlists" }, { "attributes" : { "list" : "bullet" },
+             "insert" : "\n" },
+             { "insert" : "etc" }, { "attributes" : { "list" : "bullet" }, "insert" : "\n" },
+             { "insert" : "And anything else from other folks on this webpage should show up in realtime!\n" }
+        ]}
+    }
+    //set the default contents
+    notes.remove({});
+    notes.insert(defaultNote);
+
+    //reset the contents occasionally
+    Meteor.setInterval(function(){
+        notes.remove({});
+        notes.insert(defaultNote);
+    },60000);
 });
